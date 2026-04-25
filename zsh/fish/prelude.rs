405bcfeeca892551b5087129dbb087da619c2d44
MenkeTechnologies <linux.dev25@gmail.com>
2026-04-24 21:57:11 -0400

    extract zshrs, fusevm, compsys to standalone repos
    - Removed zsh/, fusevm/, compsys/ workspace members (now at MenkeTechnologies/zshrs and MenkeTechnologies/fusevm)
    - Removed fat zshrs binary (lives in zshrs repo now)
    - Removed zshrs_shell.rs integration test
    - zsh dep now points to crates.io zshrs package
    - CI: removed zshrs-corpus and zshrs-ztst jobs (moved to zshrs repo CI)

--- zsh/fish/prelude.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::localization::{
-    localizable_consts, localizable_string, wgettext, wgettext_fmt, LocalizableString,
-};
-pub use crate::wutil::{eprintf, sprintf};
-pub use fish_widestring::prelude::*;
