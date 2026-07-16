0eb2d23c2e4f94e2c8c1250f2abad2fa2f97ffd7
MenkeTechnologies <linux.dev25@gmail.com>
2026-07-16 14:09:19 -0400

    delete extra parity

---
+++ parity/cases/20033_sprintf_hex_zero_pad.pl
@@ -0,0 +1,5 @@
+printf "%s\n", sprintf("%02x", 2);
+printf "%s\n", sprintf("%02x", 15);
+printf "%s\n", sprintf("%02x", 245);
+printf "%s\n", sprintf("%04X", 12);
+printf "%s\n", sprintf("%#04x", 5);
